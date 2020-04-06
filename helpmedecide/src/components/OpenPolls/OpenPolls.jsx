1b3571bc4c6250c669eda1633d18ce99d4cfae0c
Quinn Edwards <quinnedwards@quinns-mbp.lan>
2020-04-06 09:36:50 -0400

    Expansion Panel, Checkboxes

--- helpmedecide/src/components/OpenPolls/OpenPolls.jsx
+++ helpmedecide/src/components/OpenPolls/OpenPolls.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './OpenPolls.css';
 import Table from '@material-ui/core/Table';
-import Button from '@material-ui/core/Button';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import Button from '@material-ui/core/Button';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 class OpenPoll extends React.Component {
     constructor(props) {
@@ -32,23 +39,51 @@ class OpenPoll extends React.Component {
         })
         console.log(this.state.polls)
     }).catch(err => console.log(err))
-           
+    
     }
-        
+
     render() {
         if (this.state.polls !== undefined) {
-          
+        // const  {classes, item}  = this.props;
           return ( 
               <div>
                   <h4>Active Polls</h4>
+                  <ExpansionPanel>
+                      <ExpansionPanelSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-label="Expand"
+                      aria-controls="additional-actions1-content"
+                      id="additional-actions1-header"
+                      >
                   <Table>
                   {
-                  this.state.polls.map(function(poll, index)
+                      this.state.polls.map(function(poll, index)
                       {
                           return <TableRow key={index}><TableCell>{poll.tags}</TableCell><TableCell>{poll.question}</TableCell><TableCell>{poll.solution1}</TableCell><TableCell>{poll.solution2}</TableCell></TableRow>
                       })
                   }
                   </Table>
+                  </ExpansionPanelSummary>
+                    <FormControlLabel
+                    aria-label="Acknowledge"
+                    onClick={(event) => event.stopPropagation()}
+                    onFocus={(event) => event.stopPropagation()}
+                    control={<Checkbox />}
+                    label="checkbox"
+                    />
+                    <FormControlLabel
+                    aria-label="Acknowledge"
+                    onClick={(event) => event.stopPropagation()}
+                    onFocus={(event) => event.stopPropagation()}
+                    control={<Checkbox />}
+                    label="second checkbox"
+                    />
+                      <ExpansionPanelDetails>
+                          <Typography>
+                              Check one of the options!
+                          </Typography>
+                      </ExpansionPanelDetails>
+                  </ExpansionPanel>
               </div>
           )
       } else 
