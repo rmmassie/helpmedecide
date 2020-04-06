e969df5390fe8f1d27c6e235eac16d327fc30405
Matt Massie <rmmassie@gmail.com>
2020-04-06 15:20:43 -0400

    Revert "weekend changes"
    This reverts commit 816f9aa74b602fd29316c4c2ac24072ee3154e59.

--- helpmedecide/src/components/OpenPolls/OpenPolls.jsx
+++ helpmedecide/src/components/OpenPolls/OpenPolls.jsx
@@ -1,32 +1,28 @@
 import React from 'react';
 import './OpenPolls.css';
+import Vote from '../Vote/Vote'
 import Table from '@material-ui/core/Table';
-<<<<<<< HEAD
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import Button from '@material-ui/core/Button';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import Typography from '@material-ui/core/Typography';
-import Checkbox from '@material-ui/core/Checkbox';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-=======
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
->>>>>>> a46547f9d8ef24ca69e0ac8b855e12e43add9378
 
 class OpenPoll extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-           polls: undefined
+           polls: undefined,
+           vote: false,
+           pollId: null
         }
+        this.voteHandler = this.voteHandler.bind(this)
+       
     }
 
     componentDidMount() {
@@ -47,73 +43,67 @@ class OpenPoll extends React.Component {
             polls: pollArray
         })
         console.log(this.state.polls)
-    }).catch(err => console.log(err))
-    
+        }).catch(err => console.log(err))
+    }
+
+    voteHandler(event) {
+        console.log(event.target.value)
     }
 
     render() {
-        if (this.state.polls !== undefined) {
+        if (this.state.polls !== undefined && this.state.vote === false) {
         // const  {classes, item}  = this.props;
           return ( 
               <div>
-<<<<<<< HEAD
                   <h4>Active Polls</h4>
-                  <ExpansionPanel>
-                      <ExpansionPanelSummary
-                      expandIcon={<ExpandMoreIcon />}
-                      aria-label="Expand"
-                      aria-controls="additional-actions1-content"
-                      id="additional-actions1-header"
-                      >
-=======
-                  <TableContainer component={Paper}>
-                  <TableHead><h4>Active Polls</h4></TableHead>
-                  <TableBody>
->>>>>>> a46547f9d8ef24ca69e0ac8b855e12e43add9378
                   <Table>
-                
-                  {
+                    {
                       this.state.polls.map(function(poll, index)
                       {
-                          return <TableRow key={index}><TableCell>{poll.tags}</TableCell><TableCell>{poll.question}</TableCell><TableCell>{poll.solution1}</TableCell><TableCell>{poll.solution2}</TableCell></TableRow>
+                          return <TableRow key={index}>
+                                    <ExpansionPanel>
+                                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} aria-label="Expand" aria-controls="additional-actions1-content" id="additional-actions1-header">
+                                        {poll.question}, {poll.tags}
+                                        </ExpansionPanelSummary>
+                                        <ExpansionPanelDetails>
+                                            {/* <Typography>
+                                                Check one of the options!
+                                            </Typography> */}
+                                            <TableCell>{poll.solution1}
+                                            <RadioGroup aria-label="gender" name="Answer" value={index} >
+                                                <FormControlLabel value={poll.solution1} control={<Radio />} label={poll.solution1} />
+                                                <FormControlLabel value={poll.solution2} control={<Radio />} label={poll.solution2} />
+                                            </RadioGroup>
+                                            </TableCell>
+                                            {/* <TableCell>{poll.solution2}
+                                            <FormControlLabel
+                                                aria-label="Acknowledge"
+                                                onClick={(event) => event.stopPropagation()}
+                                                // onFocus={(event) => event.stopPropagation()}
+                                                control={<Checkbox />}
+                                                // label="second checkbox"
+                                                />
+                                            </TableCell> */}
+                                            <Button variant="contained" color="secondary">Vote!</Button>
+                                        </ExpansionPanelDetails>
+                                        </ExpansionPanel>
+                                        </TableRow>
                       })
                   }
                   </Table>
-<<<<<<< HEAD
-                  </ExpansionPanelSummary>
-                    <FormControlLabel
-                    aria-label="Acknowledge"
-                    onClick={(event) => event.stopPropagation()}
-                    onFocus={(event) => event.stopPropagation()}
-                    control={<Checkbox />}
-                    label="checkbox"
-                    />
-                    <FormControlLabel
-                    aria-label="Acknowledge"
-                    onClick={(event) => event.stopPropagation()}
-                    onFocus={(event) => event.stopPropagation()}
-                    control={<Checkbox />}
-                    label="second checkbox"
-                    />
-                      <ExpansionPanelDetails>
-                          <Typography>
-                              Check one of the options!
-                          </Typography>
-                      </ExpansionPanelDetails>
-                  </ExpansionPanel>
-=======
-                  </TableBody>
-                  </TableContainer>
->>>>>>> a46547f9d8ef24ca69e0ac8b855e12e43add9378
               </div>
           )
-      } else 
+        } else if (this.state.polls !== undefined && this.state.vote === true) {
+            return (
+                <Vote pollId={this.state.pollId} />
+            )
+        } else {
           return (
             <div>
             <p>loading Polls</p>
         </div>
-
           )
+        }
       }
     }
 
