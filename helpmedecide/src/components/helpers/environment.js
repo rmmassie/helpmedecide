ad235def55f0fb7c7da41254d3ee1ae60232f453
Stephen Lowe <stephenlowe@Stephens-MacBook-Pro.local>
2020-04-13 12:10:28 -0400

    update

--- helpmedecide/src/components/helpers/environment.js
+++ helpmedecide/src/components/helpers/environment.js
@@ -8,9 +8,9 @@ switch (window.location.hostname) {
         APIURL = 'http://localhost:3001/';
         break;
     // this is the deployed react application
-    case 'trueBooleans-helpMeDecide.herokuapp.com':
+    case 'truebooleans-helpmedecideclient.herokuapp.com':
         // this is the full url of your deployed API
-        APIURL = 'https://trueBooleans-helpMeDecide.herokuapp.com/'
+        APIURL = 'https://trueboolean-helpmedecideclient.herokuapp.com/'
 }
 
 export default APIURL;
